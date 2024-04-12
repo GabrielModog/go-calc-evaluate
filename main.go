b555d27bf230351e928d25d949c6dcb7855cb2e0
GabrielModog <criativo.modog@gmail.com>
2024-04-12 06:56:02 -0300

    [feat](unstable) Add InfixToPostfix method

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	// "strings"
 )
 
 type Stack struct {
@@ -27,22 +29,105 @@ func (s *Stack) Pop() (interface{}, bool) {
 	return lastVal, true
 }
 
-func main() {
-	fmt.Println("[ STACK TEST ]")
+func (s *Stack) Peek() (interface{}, bool) {
+	if len(s.data) == 0 {
+		return nil, false
+	}
+	return s.data[len(s.data)-1], true
+}
 
-	stk := &Stack{}
+func GetPrecedence(operator string) int {
+	precedences := map[string]int{
+		"+": 1,
+		"-": 1,
+		"*": 2,
+		"/": 2,
+		"^": 3,
+	}
+	return precedences[operator]
+}
 
-	stk.Push("First")
-	stk.Push("Second")
-	stk.Push("Last")
+func Tokenize(expression string) []string {
+	pattern := `\d+|\+|-|\*|/|\(|\)|\^`
+	token_regex, err := regexp.Compile(pattern)
 
-	fmt.Println(stk.data)
-
-	lastValue, isEmpty := stk.Pop()
-
-	if isEmpty {
-		fmt.Println("Current stack it's empty!")
+	if err != nil {
+		return []string{"ERROR"}
 	}
 
-	fmt.Println("Last value popped from stack:", lastValue)
+	return token_regex.FindAllString(expression, -1)
+}
+
+func IsNumber(str string) bool {
+	match, err := regexp.MatchString("^-?[0-9]+(?:\\.[0-9]*)?$", str)
+	if err != nil {
+		return false
+	}
+  return match
+}
+
+func InfixToPostfix(expression string) string {
+	output := &Stack{}
+	operators := &Stack{}
+	tokens := Tokenize(expression)
+
+	for _, token := range(tokens) {
+		fmt.Println("Token:", token)
+
+		// check if token is a valid number and add to stack
+		if IsNumber(token) {
+			fmt.Println("[number]:", token)
+			output.Push(token)
+		} else if token == "(" {
+			fmt.Println("[operator]:", token)
+			operators.Push(token)
+		} else if token == ")" {
+			fmt.Println("[operator]:", token)
+			for operators.data[len(operators.data)] != "(" {
+				lastVal, _ := operators.Pop()
+				output.Push(lastVal)
+			}
+		} else {
+			fmt.Println("[token]:", token)
+			fmt.Println("[stack->length]:", len(operators.data))
+
+			// valid operator order precedence
+			if len(operators.data) != 0 {
+				for GetPrecedence(operators.data[len(operators.data)-1].(string)) >= GetPrecedence(token) {
+					lastVal, _ := operators.Pop()
+					output.Push(lastVal)
+				}	
+			}
+			
+			operators.Push(token)
+		}
+	}
+
+	for len(operators.data) >= 0 {
+		lastVal, _ := operators.Pop()
+		output.Push(lastVal)
+	}
+
+	fmt.Println(output.data)
+
+	return ""
+	// return strings.Join(output.data, " ")
+}
+
+func main() {
+	tokens := InfixToPostfix("2 + 3")
+
+	// output := &Stack{}
+	// operators := &Stack{}
+
+	// output.Push(2)
+	// operators.Push("+")
+	// output.Push(3)
+
+	// fmt.Println(output.data)
+	// fmt.Println(operators.data)
+
+
+	fmt.Println(tokens)
+
 }
