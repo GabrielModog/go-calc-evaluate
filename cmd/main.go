b2576c964a127e3a3a3512598512011ecf2b8358
GabrielModog <modog.dev@gmail.com>
2024-05-16 13:57:32 -0300

    [fix] Change scan to bufio scanner

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 
 	parser "github.com/GabrielModog/go-calc-evaluate/shunting_yard"
 )
 
 func main() {
-	expression := ""
-
+	scanner := bufio.NewScanner(os.Stdin)
+	
 	fmt.Print("= ")
-	fmt.Scanln(&expression)
 
-	if strings.TrimSpace(expression) == "" {
-		panic("insufficient expressions")
-	}
+	scanner.Scan()
+	expressionInput := scanner.Text()
+
+	expression := strings.TrimSpace(expressionInput)
+
+	fmt.Println("expression:",expression)
+
+	fmt.Println("input:", expressionInput)
 
 	tokens, _ := parser.InfixToPostfix(expression)
 	result, _ := parser.EvaluatePostfix(tokens)
