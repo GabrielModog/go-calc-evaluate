7bd54279dfafce2f27a71fd6b1b96ec0b759b330
GabrielModog <modog.dev@gmail.com>
2024-06-23 11:24:52 -0300

    [refactor] Change main file making able to use the CLI app in two different ways: executing or passing the filepath.

--- cmd/main.go
+++ cmd/main.go
@@ -3,24 +3,66 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
 	parser "github.com/GabrielModog/go-evaluate-expressions/shunting_yard"
 )
 
+func usage() {
+	fmt.Println("\nUsage:\n- calcexpr <pathname_to_file.txt>\n- or execute program CLI.")
+}
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	
-	fmt.Print("= ")
+	args := os.Args[1:]
 
-	scanner.Scan()
-	expressionInput := scanner.Text()
+	if len(args) != 1 {
+		usage()
 
-	expression := strings.TrimSpace(expressionInput)
+		scanner := bufio.NewScanner(os.Stdin)
 
-	tokens, _ := parser.InfixToPostfix(expression)
-	result, _ := parser.EvaluatePostfix(tokens)
+		fmt.Print("= ")
 
-	fmt.Println(result)
+		scanner.Scan()
+		expressionInput := scanner.Text()
+
+		expression := strings.TrimSpace(expressionInput)
+
+		tokens, _ := parser.InfixToPostfix(expression)
+		result, _ := parser.EvaluatePostfix(tokens)
+
+		fmt.Println(result)
+		os.Exit(1)
+	}
+
+	path := args[0]
+	checkExtension := strings.LastIndex(path, ".txt")
+
+	if checkExtension == -1 {
+		usage()
+	}
+
+	data, err := os.ReadFile(path)
+	check(err)
+
+	expressions := strings.Split(string(data), "\n")
+
+	for i := range expressions {
+		exp := strings.TrimSpace(expressions[i])
+		tokens, _ := parser.InfixToPostfix(exp)
+		evaluated, _ := parser.EvaluatePostfix(tokens)
+
+		result := fmt.Sprintf("- %s = %.2f", exp, evaluated)
+
+		fmt.Println(result)
+	}
+
+	os.Exit(1)
 }
